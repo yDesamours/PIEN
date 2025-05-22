8264eaafe2a7588146f8fdc5c87213c6aa6d8bd0
ydesamoursTurbo <yvelt.desamours@groupturbo.com>
2025-05-22 19:58:30 -0400

    user jwt generation

--- services/go/utilisateur/handler.go
+++ services/go/utilisateur/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func createUsers(app *internal.App, repo *UtilisateurRepository) gin.HandlerFunc {
@@ -16,6 +17,21 @@ func createUsers(app *internal.App, repo *UtilisateurRepository) gin.HandlerFunc
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+
+		_, err = repo.findByEmail(user.Email)
+		if err != nil {
+			app.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.MotDePasse), 20)
+		if err != nil {
+			app.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		user.MotDePasse = string(hashedPassword)
 
 		repo.create(user)
 
@@ -33,5 +49,53 @@ func updateUser(app *internal.App, repo *UtilisateurRepository) gin.HandlerFunc 
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+	}
+}
+
+func getUser(app *internal.App, repo *UtilisateurRepository) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userID := c.Param("id")
+		user, err := repo.findById(userID)
+		if err != nil {
+			app.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+func login(app *internal.App, repo *UtilisateurRepository) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		type login struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+			Role     string `json:"role"`
+		}
+
+		var credentials login
+		err := c.BindJSON(&credentials)
+		if err != nil {
+			app.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		user, err := repo.findByEmailAndRole(credentials.Email, credentials.Role)
+		if err != nil {
+			app.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(credentials.Password), []byte(user.MotDePasse))
+		if err != nil {
+			app.Error(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
 	}
 }
--- services/go/utilisateur/model.go
+++ services/go/utilisateur/model.go
@@ -1,14 +1,22 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Utilisateur struct {
-	ID         string
-	Nom        string
-	Prenom     string
-	Email      string
-	MotDePasse string
-	Role       string
+	ID                      string `gorm:"primaryKey"`
+	Email                   string
+	MotDePasse              string
+	Role                    string
+	Telephone               string
+	is2FaActive             string
+	Statut                  string
+	NotificationsAutorisees []string
+	CreeLe                  time.Time
+	CreePar                 string
 }
 
 type UtilisateurRepository struct {
@@ -19,18 +27,80 @@ func newUtilisateurRepository(db *gorm.DB) *UtilisateurRepository {
 	return &UtilisateurRepository{db}
 }
 
-func (r *UtilisateurRepository) create(u *Utilisateur) {
-	r.db.Create(u)
+func (r *UtilisateurRepository) create(u *Utilisateur) error {
+	result := r.db.Create(u)
+	return result.Error
 
 }
 
-func (r *UtilisateurRepository) update(u *Utilisateur) {
-	r.db.Save(u)
+func (r *UtilisateurRepository) update(u *Utilisateur) error {
+	result := r.db.Save(u)
+	return result.Error
 }
 
-func (r *UtilisateurRepository) findById(ID string) *Utilisateur {
+func (r *UtilisateurRepository) findById(ID string) (*Utilisateur, error) {
 	var Utilisateur Utilisateur
-	r.db.First(&Utilisateur, ID)
+	result := r.db.First(&Utilisateur, ID)
 
-	return &Utilisateur
+	return &Utilisateur, result.Error
+}
+
+func (r *UtilisateurRepository) findByEmailAndRole(email, role string) (*Utilisateur, error) {
+	var utilisateur Utilisateur
+	result := r.db.Where("email = ? and role = ?", email, role).First(&utilisateur)
+	return &utilisateur, result.Error
+}
+
+func (r *UtilisateurRepository) findByEmail(email string) (*Utilisateur, error) {
+	var utilisateur Utilisateur
+	result := r.db.Where("email = ?", email).First(&utilisateur)
+	return &utilisateur, result.Error
+}
+
+type Jeton struct {
+	ID            string
+	UtilisateurID string
+	Valeur        string
+	Scope         string
+	EmisLe        time.Time
+	ExpireLe      time.Time
+	EstRevoque    bool
+	ipAdresse     string
+	userAgent     string
+}
+
+type JetonRepository struct {
+	db *gorm.DB
+}
+
+func (repository *JetonRepository) create(jeton *Jeton) error {
+	result := repository.db.Create(jeton)
+	return result.Error
+}
+
+func (repository *JetonRepository) update(jeton *Jeton) error {
+	result := repository.db.Save(jeton)
+	return result.Error
+}
+
+type HistoriqueMotDePasse struct {
+	ID            int
+	UtilisateurID string
+	MotDePasse    string
+	ChangeLe      time.Time
+}
+
+type HistoriqueMotDePasseRepository struct {
+	db *gorm.DB
+}
+
+func (r *HistoriqueMotDePasseRepository) create(historiqueMotDePasse *HistoriqueMotDePasse) error {
+	result := r.db.Create(historiqueMotDePasse)
+	return result.Error
+}
+
+func (r *HistoriqueMotDePasseRepository) getByUserID(utilisateurID string) ([]HistoriqueMotDePasse, error) {
+	var historiques []HistoriqueMotDePasse
+	result := r.db.Where("utilisateur_id = ?", utilisateurID).Find(&historiques)
+	return historiques, result.Error
 }
